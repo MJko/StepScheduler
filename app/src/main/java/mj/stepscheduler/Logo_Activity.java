2a067b42c32d5b879741b02b4a9a1b711560302b
MJKO <gmj1197@nate.com>
2016-11-07 22:48:32 +0900

    Add app

--- app/src/main/java/mj/stepscheduler/Logo_Activity.java
+++ app/src/main/java/mj/stepscheduler/Logo_Activity.java
@@ -1,13 +1,39 @@
 package mj.stepscheduler;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.*;
+import android.app.Activity;
+import android.content.*;
+import android.view.*;
 
-public class Logo_Activity extends AppCompatActivity {
+public class Logo_Activity extends Activity {
+
+    Handler h;//핸들러 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //인트로화면이므로 타이틀바를 없앤다
         setContentView(R.layout.logo_activity);
+        h= new Handler(); //딜래이를 주기 위해 핸들러 생성
+        h.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
     }
+
+    Runnable mrun = new Runnable(){
+        @Override
+        public void run(){
+            Intent i = new Intent(Logo_Activity.this, App_Main_Activity.class); //인텐트 생성(현 액티비티, 새로 실행할 액티비티)
+            startActivity(i);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            //overridePendingTransition 이란 함수를 이용하여 fade in,out 효과를줌. 순서가 중요
+        }
+    };
+    //인트로 중에 뒤로가기를 누를 경우 핸들러를 끊어버려 아무일 없게 만드는 부분
+    //미 설정시 인트로 중 뒤로가기를 누르면 인트로 후에 홈화면이 나옴.
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        h.removeCallbacks(mrun);
+    }
+
 }
