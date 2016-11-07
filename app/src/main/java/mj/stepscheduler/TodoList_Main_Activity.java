477e2565248d40ba09a22f79acac31332a8333af
MJKO <gmj1197@nate.com>
2016-11-07 21:14:02 +0900

    build.gradle

---
+++ app/src/main/java/mj/stepscheduler/TodoList_Main_Activity.java
@@ -0,0 +1,13 @@
+package mj.stepscheduler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TodoList_Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todolist_main_activity);
+    }
+}
